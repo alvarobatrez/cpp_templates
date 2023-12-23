eae5e87a5e5c70fcd3d705f8d508c0b71aca8bcb
alvarobatrez <alvarobatrez27@gmail.com>
2023-12-22 19:44:54 -0600

    overload operators

---
+++ OverloadingOperators/InsertionExtractionOperators/src/MyString.cpp
@@ -0,0 +1,122 @@
+#include "MyString.hpp"
+#include <iostream>
+
+MyString::MyString()
+: str {nullptr}
+{
+    std::cout << "no arguments constructor" << std::endl;
+    str = new char [1];
+    *str = '\0';
+}
+
+MyString::MyString(const char *s)
+: str {nullptr}
+{
+    std::cout << "overloaded constructor" << std::endl;
+    if (s == nullptr)
+    {
+        str = new char [1];
+        *str = '\0';
+    }
+    else
+    {
+        str = new char [std::strlen(s) + 1];
+        std::strcpy(str, s);
+    }
+}
+
+MyString::MyString(const MyString &source)
+: str {nullptr}
+{
+    std::cout << "copy constructor" << std::endl;
+    str = new char [std::strlen(source.str) + 1];
+    std::strcpy(str, source.str);
+}
+
+MyString::MyString(MyString &&source)
+: str {source.str}
+{
+    std::cout << "move constructor" << std::endl;
+    source.str = nullptr;
+}
+
+MyString::~MyString()
+{
+    std::cout << "destructor: " << &str << std::endl;
+    delete [] str;
+}
+
+MyString &MyString::operator=(const MyString &rhs)
+{
+    std::cout << "copy assignment" << std::endl;
+    if (this == &rhs)
+    {
+        return *this;
+    }
+    delete [] str;
+    str = new char [std::strlen(rhs.str) + 1];
+    std::strcpy(str, rhs.str);
+    return *this;
+}
+
+MyString &MyString::operator=(MyString &&rhs)
+{
+    std::cout << "move assignment" << std::endl;
+    if (this == &rhs)
+    {
+        return *this;
+    }
+    delete [] str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+
+MyString MyString::operator-() const
+{
+    std::cout << "make lowercase" << std::endl;
+    char *buff = new char [std::strlen(str) + 1];
+    std::strcpy(buff, str);
+
+    for (size_t i {}; i < std::strlen(buff); i++)
+    {
+        buff[i] = std::tolower(buff[i]);
+    }
+
+    MyString aux {buff};
+    delete [] buff;
+    return aux;
+}
+
+MyString MyString::operator+(const MyString &rhs) const
+{
+    std::cout << "concatenate" << std::endl;
+    char *buff = new char [std::strlen(str) + 1];
+    std::strcpy(buff, str);
+    std::strcat(buff, rhs.str);
+
+    MyString aux {buff};
+    delete [] buff;
+    return aux;
+}
+
+bool MyString::operator==(const MyString &rhs) const
+{
+    std::cout << "equal" << std::endl;
+    return (std::strcmp(str, rhs.str) == 0);
+}
+
+size_t MyString::get_length() const
+{
+    return std::strlen(str);
+}
+
+const char *MyString::get_string() const
+{
+    return str;
+}
+
+void MyString::display() const
+{
+    std::cout << "string = " << str << ", length = " << get_length() << std::endl;
+}
---
+++ OverloadingOperators/InsertionExtractionOperators/src/MyString.hpp
@@ -0,0 +1,59 @@
+#ifndef _MYSTRING_HPP_
+#define _MYSTRING_HPP_
+
+#include <cstring>
+
+class MyString
+{
+    private:
+
+    char *str; // C-style string
+
+    public:
+
+    // ***** NO ARGUMENTS CONSTRUCTOR *****
+
+    MyString();
+
+    // ***** OVERLOADED CONSTRUCTOR *****
+
+    MyString(const char *s);
+
+    // ***** COPY CONSTRUCTOR *****
+
+    MyString(const MyString &source);
+
+    // ***** MOVE CONSTRUCTOR *****
+
+    MyString(MyString &&source);
+
+    // ***** DESTRUCTOR *****
+
+    ~MyString();
+
+    // ***** COPY ASSIGNMENT *****
+
+    MyString &operator=(const MyString &rhs);
+
+    // ***** MOVE ASSIGNMENT *****
+
+    MyString &operator=(MyString &&rhs);
+
+    // ***** MAKE LOWERCASE *****
+
+    MyString operator-() const;
+
+    // ***** CONCATENATE *****
+
+    MyString operator+(const MyString &rhs) const;
+
+    // ***** EQUAL *****
+
+    bool operator==(const MyString &rhs) const;
+
+    size_t get_length() const;
+    const char *get_string() const;
+    void display() const;
+};
+
+#endif
