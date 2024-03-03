d83b38d3f544e2184007f81d678ef3afab4daf99
alvarobatrez <alvarobatrez27@gmail.com>
2024-03-02 18:26:38 -0600

    bug

--- STL/Macros/src/main.cpp
+++ STL/Macros/src/main.cpp
@@ -3,22 +3,22 @@
 // ***** MACROS *****
 
 #define PI 3.1416
-#define e 2.71828
+#define e 2.7182
 
 #define MIN(x, y) ((x < y) ? x : y)
 
-#define SQRT(x) (x * x)
+#define POW2(x) (x * x)
 
 int main()
 {
-    std::cout << "Pi = " << PI << std::endl;
-    std::cout << "e = " << e << std::endl;
+    std::cout << PI << std::endl;
+    std::cout << e << std::endl;
     
     std::cout << MIN(4.5, 4.3) << std::endl;
     std::cout << MIN(5, 7) << std::endl;
     std::cout << MIN('d', 'b') << std::endl;
 
-    std::cout << 100 / SQRT(5) << std::endl;
+    std::cout << 100 / POW2(5) << std::endl;
     
     return 0;
 }
